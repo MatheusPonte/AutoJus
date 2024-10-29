a1a645bc135ed481d5250c3c65ba04e07359720d
matheusponte <matheuspontes894@outlook.com>
2024-10-29 07:26:06 -0300

    Third function

--- automacao.py
+++ automacao.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import sync_playwright
 from dotenv import load_dotenv
 import time
-
+import pyautogui as pya
 import os
 from tkinter import *
 
@@ -30,6 +30,17 @@ def Pesquisar(p):
     p.locator('//*[@id="content"]/div/div[3]/div/div/div[2]/div/div[1]/div/div/div[2]/div/button/span').click()
     time.sleep(5)
 
+def Desparo(p):
+    p.locator('//*[@id="processosFiltroEtiquetas"]/div/div/div/div[1]/div[2]/div/i').click()
+    time.sleep(3)
+    print('passei aq')
+    img = pya.locateOnScreen('img/pesquisar.png', confidence=0.7)
+    print('passei')
+    pya.click(img)
+    p.keyboard.type('7') #7Setembro
+    p.locator('//*[@id="app"]/div[13]/div/div/div[2]/a/div[1]/div/div/div[1]/div/div').click()
+    time.sleep(3)
+
 def rodar_automacao():
     with sync_playwright() as p:
         navegador = p.chromium.launch(headless=False)
@@ -37,6 +48,7 @@ def rodar_automacao():
         pagina = navegador.new_page()
         Logar(pagina, userw, passw)
         Pesquisar(pagina)
+        Desparo(pagina)
 
 
 janela = Tk()
